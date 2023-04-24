b1f3c15349eece0b57cd8fe304dbf9eec834fbc3
MaestroKwon <no1con@naver.com>
2023-04-24 10:21:15 +0900

    update

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
-#-*-coding:UTF-8-*-
+# -*- coding:UTF-8 -*-
 import streamlit as st
 
 def main():
-	st.title("HelloWorld")
+    st.title("Hello World")
+
+    # text
+    st.text('This is so {}'.format("good"))
+
+    # Header
+    st.header('This is Header')
+
+    # subheader
+    st.subheader('This is subHeader')
+
+    # Markdown
+    st.markdown('## This is Markdown')
+
+    # 색상이 들어간 텍스트 feature
+    st.success('성공')
+    st.warning('경고')
+    st.info('정보롸 관련된 탭')
+    st.error('에러 메세지')
+    st.exception('예외처리')
+
+    # st.write()
+    st.write('일반텍스트')
+    st.write(1+2)
+    st.write(dir(str))
+
+    st.title(':sunglasses:')
+
+    # help
+    st.help(range)
+    st.help(st.title)
+
 
 if __name__ == '__main__':
     main()
